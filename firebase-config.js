b1e0666aaa22724ed68a73c3df4af174bc1ea811
손경희 <wonzia80@gmail.com>
2026-05-22 15:08:45 +0900

    SaaS Firebase 연동 및 빌드 ID 분리

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // soccer-lesson-app-public 웹 앱 설정
 window.FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyDw3B7X4WY8kyUg61HOZNuLbZmOPXc0_fI',
-  authDomain: 'soccer-lesson-app-public.firebaseapp.com',
-  projectId: 'soccer-lesson-app-public',
-  storageBucket: 'soccer-lesson-app-public.firebasestorage.app',
-  messagingSenderId: '133769139294',
-  appId: '1:133769139294:web:f3d7f36d46487cddc7d0cf'
+  apiKey: "AIzaSyCVpw06hpU1CYQCvfMggCVenGCbNO-ftlE",
+  authDomain: "soccer-lesson-manager-saas.firebaseapp.com",
+  projectId: "soccer-lesson-manager-saas",
+  storageBucket: "soccer-lesson-manager-saas.firebasestorage.app",
+  messagingSenderId: "514169699034",
+  appId: "1:514169699034:web:dcea6576b1c004477c03e3",
 };
